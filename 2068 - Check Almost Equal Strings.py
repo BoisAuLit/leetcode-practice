f84ad09dc331270065c160e8c3914183e6275399
Bohao LI <arthur130237@gmail.com>
2023-07-05 00:22:28 -0400

    2068 - Check Almost Equal Strings

---
+++ 2068 - Check Almost Equal Strings.py
@@ -0,0 +1,48 @@
+"""
+Time complexity: O(n)
+Space complexity: O(n)
+"""
+
+
+class Solution:
+    def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
+        mapping_1 = {}
+        for character in word1:
+            if character in mapping_1:
+                mapping_1[character] += 1
+            else:
+                mapping_1[character] = 1
+        for character in word2:
+            if character in mapping_1:
+                mapping_1[character] -= 1
+            else:
+                mapping_1[character] = -1
+        return all(abs(i) <= 3 for i in mapping_1.values())
+
+
+s = Solution()
+
+# Expecting False
+# word1 = "aaaa"
+# word2 = "bccb"
+
+# Expecting True
+# word1 = "abcdeef"
+# word2 = "abaaacc"
+
+# Expecting True
+word1 = "cccddabba"
+word2 = "babababab"
+
+result = s.checkAlmostEquivalent(word1, word2)
+print(result)
+
+"""
+Runtime
+- 49 ms
+- Beats 55.93%
+
+Memory
+- 16.4 MB
+- Beats 36.81%
+"""
